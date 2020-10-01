b521aa164876257beaed238769f71cdd9ea96b0e
Doha Khaled <Dkhaled@integrant.com>
2020-10-01 10:09:43 +0200

    first commit

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace ConsoleCalculator
+{
+    public class Calculator
+    {
+        public int Calculate(int number1, int number2, string operation)
+        {
+
+            /*
+             * this for Try & catch & finally 
+             */
+            //string nonNullOperation =  operation ?? throw new ArgumentNullException(nameof(operation));
+            ////if (operation is null)
+            ////{
+            ////    throw new ArgumentNullException(nameof(operation));
+            ////}
+            //if (nonNullOperation == "/")
+            //{
+            //    return Divide(number1, number2);
+            //}
+            //else
+            //{
+            //    throw new ArgumentOutOfRangeException(nameof(operation),"The mathematical operator is not supported.");
+            //    //Console.WriteLine("Unknown operation.");
+            //    //return 0;
+            //}
+
+
+            //RETHROWING EXCEPTIONS
+            //string nonNullOperation = operation ?? throw new ArgumentNullException(nameof(operation));
+            //if (nonNullOperation == "/")
+            //{
+            //    try
+            //    {
+            //        return Divide(number1, number2);
+            //    }
+            //    catch(DivideByZeroException ex )
+            //    {
+            //        //Log.Error(ex);
+            //        throw;
+            //    }
+            //}
+            //else
+            //{
+            //    throw new ArgumentOutOfRangeException(nameof(operation), "The mathematical operator is not supported.");
+            //}
+
+
+            // WRAPPING EXCEPTIONS means wraaping a small exception in a bigger one ArithmeticException will wrap DividedByZeroException
+            //string nonNullOperation = operation ?? throw new ArgumentNullException(nameof(operation));
+            //if (nonNullOperation == "/")
+            //{
+            //    try
+            //    {
+            //        return Divide(number1, number2);
+            //    }
+            //    catch (DivideByZeroException ex)
+            //    {
+            //        //Our general catch block executes with Sorry, something went wrong, but this time the type of the exception is ArithmeticException; 
+            //        //however, notice as before, we've lost valuable stack trace information, 
+            //        //and once again we're losing the fact that the original exception occurred inside the Divide method. 
+            //        //So let's hit Enter to exit, and we'll go and fix this up. Recall earlier in the course when we discussed the System. 
+            //        //Exception type, we learned about a number of different constructors. 
+            //        //One of these constructors takes a message and also allows us to specify an innerException. 
+            //        //So let's modify this ArithmeticException constructor. We'll start off by providing a message,
+            //        //so we'll just say that an error occurred during calculation, and as a second parameter here, we can specify an innerException. 
+            //        throw new ArithmeticException("An error occured during calculations", ex);
+            //    }
+            //}
+            //else
+            //{
+            //    throw new ArgumentOutOfRangeException(nameof(operation), "The mathematical operator is not supported.");
+            //}
+
+            // FILTERING CATCHES 
+            string nonNullOperation = operation ?? throw new ArgumentNullException(nameof(number1));
+            if (nonNullOperation == "/")
+            {
+                try
+                {
+                    return Divide(number1, number2);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    throw new ArithmeticException("An error occured during calculations", ex);
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(operation), "The mathematical operator is not supported.");
+            }
+
+
+        }
+
+        private int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
+
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine());
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpperInvariant();
+
+
+            var calculator = new Calculator();
+
+            //Try & Catchs Only
+            //try
+            //{
+            //    int result = calculator.Calculate(number1, number2, operation);
+            //    DisplayResult(result);
+            //}
+            //catch (ArgumentNullException ex)
+            //{
+            //    // Log.Error(ex);
+            //    WriteLine($"Operation was not provided. {ex}");
+            //}
+            //catch (ArgumentOutOfRangeException ex)
+            //{
+            //    // Log.Error(ex);
+            //    WriteLine($"Operation is not supported. {ex}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    WriteLine($"Sorry, something went wrong. {ex}");
+            //}
+
+
+            //(Try & Catches and Finally) & (RETHROWING EXCEPTIONS) & WRAPPING EXCEPTIONS
+            //try
+            //{
+            //    int result = calculator.Calculate(number1, number2, operation);
+            //    DisplayResult(result);
+            //}
+            //catch (ArgumentNullException ex)
+            //{
+            //    // Log.Error(ex);
+            //    WriteLine($"Operation was not provided. {ex}");
+            //}
+            //catch (ArgumentOutOfRangeException ex)
+            //{
+            //    // Log.Error(ex);   
+            //    WriteLine($"Operation is not supported. {ex}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    WriteLine($"Sorry, something went wrong. {ex}");
+            //}
+            //finally
+            //{
+            //    WriteLine("......Finally.....");
+            //}
+
+            //(Filtering CAtCHES)
+            //try
+            //{
+            //    int result = calculator.Calculate(number1, number2, null);
+            //    DisplayResult(result);
+            //}
+            //catch (ArgumentNullException ex) when (ex.ParamName == "operation")
+            //{
+            //    // Log.Error(ex);
+            //    WriteLine($"Operation was not provided. {ex}");
+            //}
+            //catch (ArgumentNullException ex) 
+            //{
+            //    // Log.Error(ex);
+            //    WriteLine("An Argument was null");
+            //}
+            //catch (ArgumentOutOfRangeException ex)
+            //{
+            //    // Log.Error(ex);   
+            //    WriteLine($"Operation is not supported. {ex}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    WriteLine($"Sorry, something went wrong. {ex}");
+            //}
+            //finally
+            //{
+            //    WriteLine("......Finally.....");
+            //}
+
+            //(Global Handled Exceptions)
+            try
+            {
+                int result = calculator.Calculate(number1, number2, operation);
+                DisplayResult(result);
+            }
+            catch (ArgumentNullException ex) when (ex.ParamName == "operation")
+            {
+                // Log.Error(ex);
+                WriteLine($"Operation was not provided. {ex}");
+            }
+            catch (ArgumentNullException ex)
+            {
+                // Log.Error(ex);
+                WriteLine("An Argument was null");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // Log.Error(ex);   
+                WriteLine($"Operation is not supported. {ex}");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Sorry, something went wrong. {ex}");
+            }
+            finally
+            {
+                WriteLine("......Finally.....");
+            }
+
+
+
+
+
+
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(int result)
+        {
+            WriteLine($"Result is: {result}");
+        }
+    }
+}
